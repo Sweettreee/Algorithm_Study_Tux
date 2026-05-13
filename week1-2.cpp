1310896a7521c4e35d46957f3dad4066dda20ef7
wldn <myull425@naver.com>
2026-05-13 20:51:52 +0900

    <<Feat>>:<<회문>>

--- week1-2.cpp
+++ week1-2.cpp
@@ -1,14 +1,14 @@
-class Solution {
-public:
-    bool isPalindrome(string s) {
-       string newStr;
-       for(int i=0; i<s.size(); i++){
-        if(isalnum(s[i])){
-            newStr += tolower(s[i]);
-        }
-       }
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//        string newStr;
+//        for(int i=0; i<s.size(); i++){
+//         if(isalnum(s[i])){
+//             newStr += tolower(s[i]);
+//         }
+//        }
        
-       return newStr == string(newStr.rbegin(), newStr.rend()); //newStr을 역순으로 만드는 String.
+//        return newStr == string(newStr.rbegin(), newStr.rend()); //newStr을 역순으로 만드는 String.
 
-}
-};
+// }
+// };
